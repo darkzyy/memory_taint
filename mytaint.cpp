18552d1e2499341c3e0f7863ec4f5b02d2b91e70
Zhou Yaoyang <diamondzyy@sina.com>
2016-12-19 10:16:13 +0800

    modified registers to satisfy IA32

--- mytaint.cpp
+++ mytaint.cpp
@@ -57,44 +57,36 @@ bool taintReg(REG reg)
 
     switch(reg){
 
-        case REG_RAX:  regsTainted.push_front(REG_RAX);
         case REG_EAX:  regsTainted.push_front(REG_EAX);
         case REG_AX:   regsTainted.push_front(REG_AX);
         case REG_AH:   regsTainted.push_front(REG_AH);
         case REG_AL:   regsTainted.push_front(REG_AL);
                        break;
 
-        case REG_RBX:  regsTainted.push_front(REG_RBX);
         case REG_EBX:  regsTainted.push_front(REG_EBX);
         case REG_BX:   regsTainted.push_front(REG_BX);
         case REG_BH:   regsTainted.push_front(REG_BH);
         case REG_BL:   regsTainted.push_front(REG_BL);
                        break;
 
-        case REG_RCX:  regsTainted.push_front(REG_RCX);
         case REG_ECX:  regsTainted.push_front(REG_ECX);
         case REG_CX:   regsTainted.push_front(REG_CX);
         case REG_CH:   regsTainted.push_front(REG_CH);
         case REG_CL:   regsTainted.push_front(REG_CL);
                        break;
 
-        case REG_RDX:  regsTainted.push_front(REG_RDX);
         case REG_EDX:  regsTainted.push_front(REG_EDX);
         case REG_DX:   regsTainted.push_front(REG_DX);
         case REG_DH:   regsTainted.push_front(REG_DH);
         case REG_DL:   regsTainted.push_front(REG_DL);
                        break;
 
-        case REG_RDI:  regsTainted.push_front(REG_RDI);
         case REG_EDI:  regsTainted.push_front(REG_EDI);
         case REG_DI:   regsTainted.push_front(REG_DI);
-        case REG_DIL:  regsTainted.push_front(REG_DIL);
                        break;
 
-        case REG_RSI:  regsTainted.push_front(REG_RSI);
         case REG_ESI:  regsTainted.push_front(REG_ESI);
         case REG_SI:   regsTainted.push_front(REG_SI);
-        case REG_SIL:  regsTainted.push_front(REG_SIL);
                        break;
 
         default:
@@ -111,44 +103,36 @@ bool removeRegTainted(REG reg)
 {
     switch(reg){
 
-        case REG_RAX:  regsTainted.remove(REG_RAX);
         case REG_EAX:  regsTainted.remove(REG_EAX);
         case REG_AX:   regsTainted.remove(REG_AX);
         case REG_AH:   regsTainted.remove(REG_AH);
         case REG_AL:   regsTainted.remove(REG_AL);
                        break;
 
-        case REG_RBX:  regsTainted.remove(REG_RBX);
         case REG_EBX:  regsTainted.remove(REG_EBX);
         case REG_BX:   regsTainted.remove(REG_BX);
         case REG_BH:   regsTainted.remove(REG_BH);
         case REG_BL:   regsTainted.remove(REG_BL);
                        break;
 
-        case REG_RCX:  regsTainted.remove(REG_RCX);
         case REG_ECX:  regsTainted.remove(REG_ECX);
         case REG_CX:   regsTainted.remove(REG_CX);
         case REG_CH:   regsTainted.remove(REG_CH);
         case REG_CL:   regsTainted.remove(REG_CL);
                        break;
 
-        case REG_RDX:  regsTainted.remove(REG_RDX);
         case REG_EDX:  regsTainted.remove(REG_EDX);
         case REG_DX:   regsTainted.remove(REG_DX);
         case REG_DH:   regsTainted.remove(REG_DH);
         case REG_DL:   regsTainted.remove(REG_DL);
                        break;
 
-        case REG_RDI:  regsTainted.remove(REG_RDI);
         case REG_EDI:  regsTainted.remove(REG_EDI);
         case REG_DI:   regsTainted.remove(REG_DI);
-        case REG_DIL:  regsTainted.remove(REG_DIL);
                        break;
 
-        case REG_RSI:  regsTainted.remove(REG_RSI);
         case REG_ESI:  regsTainted.remove(REG_ESI);
         case REG_SI:   regsTainted.remove(REG_SI);
-        case REG_SIL:  regsTainted.remove(REG_SIL);
                        break;
 
         default:
